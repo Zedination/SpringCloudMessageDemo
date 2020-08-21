f86965c5168a38ddbdc6b72aed01e4134857c8b0
Zedination <50323429+Zedination@users.noreply.github.com>
2020-08-22 01:03:20 +0700

    test selenium + description Google Guava

--- src/main/java/com/example/restcontroller/ThongBao.java
+++ src/main/java/com/example/restcontroller/ThongBao.java
@@ -1,6 +1,8 @@
 package com.example.restcontroller;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +37,29 @@ public class ThongBao {
 		driver.getTitle();
 	}
 	@GetMapping("/demo-selenium")
-	public String semoSelenium() {
-		return "aaaa";
+	public String semoSelenium() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",System.getenv("CHROMEDRIVER_PATH"));
+		ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--remote-debugging-port=9222");
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		WebDriver driver = new ChromeDriver();
+		String url = "http://sis.utc.edu.vn/";
+		driver.get(url);
+		driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/a")).click();
+		WebElement inputUsername = driver.findElement(By.xpath("//*[@id=\"i0116\"]"));
+		inputUsername.sendKeys("duc171201239@st.utc.edu.vn");
+		WebElement buttonNext = driver.findElement(By.xpath("//*[@id=\"idSIButton9\"]"));
+		buttonNext.click();
+		WebElement inputPass = driver.findElement(By.xpath("//*[@id=\"i0118\"]"));
+		inputPass.sendKeys("dagevjul14");
+		Thread.sleep(1000);
+		WebElement buttonSubmit = driver.findElement(By.xpath("//*[@id=\"idSIButton9\"]"));
+		buttonSubmit.click();
+		WebElement buttonConfirm = driver.findElement(By.xpath("//*[@id=\"idBtn_Back\"]"));
+		Thread.sleep(1000);
+		buttonConfirm.click();
+		return driver.getTitle();
 	}
 }
