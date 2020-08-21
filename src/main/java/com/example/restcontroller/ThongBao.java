9eca43b87149e709f32757dbc2073eeee7cebb31
Zedination <50323429+Zedination@users.noreply.github.com>
2020-08-21 23:16:27 +0700

    Update ThongBao.java

--- src/main/java/com/example/restcontroller/ThongBao.java
+++ src/main/java/com/example/restcontroller/ThongBao.java
@@ -1,6 +1,7 @@
 package com.example.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,9 @@ public class ThongBao {
 	public String sendThongBao(@RequestParam("fcmtoken") String fcmtoken, @RequestParam("content") String content) {
 		System.out.println(fcmtoken);
 		return fcmService.pushNotification(fcmtoken, content);
+	}
+	@GetMapping("/get-env")
+	public String getEnv() {
+		return System.getenv("GOOGLE_CHROME_PATH") +" / "+System.getenv("CHROMEDRIVER_PATH");
 	}
 }
